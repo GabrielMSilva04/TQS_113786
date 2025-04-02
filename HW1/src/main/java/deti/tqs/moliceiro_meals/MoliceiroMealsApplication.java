f88a96b58a0090a62e0cd7ea7463d094f74a80ba
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-04-02 18:52:14 +0100

    initial hw1 setup

---
+++ HW1/src/main/java/deti/tqs/moliceiro_meals/MoliceiroMealsApplication.java
@@ -0,0 +1,13 @@
+package deti.tqs.moliceiro_meals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoliceiroMealsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoliceiroMealsApplication.class, args);
+	}
+
+}
